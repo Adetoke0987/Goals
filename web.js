a1837970bbf6dbf9f6d37f0b3cf59b2f075d656a
Adetoke0987 <awesometokeh@gmail.com>
2024-07-03 23:26:57 +0100

    update

--- web.js
+++ web.js
@@ -1,27 +1,27 @@
-
 function updateCurrentTime() {
     const currentTimeElement = document.getElementById('currentTimeUTC');
-    const currentTimeUTC = new Date().toUTCString();
-    currentTimeElement.textContent = currentTimeUTC;
+    const now = new Date();
+    const utcString = now.getUTCHours().toString().padStart(2, '0') + ':' +
+                      now.getUTCMinutes().toString().padStart(2, '0') + ':' +
+                      now.getUTCSeconds().toString().padStart(2, '0');
+    currentTimeElement.textContent = utcString;
 }
-
 
 function updateCurrentDay() {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const currentDayElement = document.getElementById('currentDay');
-    const currentDayIndex = new Date().getDay();
+    const now = new Date();
+    const currentDayIndex = now.getUTCDay(); 
     const currentDay = daysOfWeek[currentDayIndex];
     currentDayElement.textContent = currentDay;
 }
-
 
 document.addEventListener('DOMContentLoaded', function() {
     updateCurrentTime();
     updateCurrentDay();
 
-   
     setInterval(function() {
         updateCurrentTime();
         updateCurrentDay();
-    }, 60000); 
+    }, 1000); 
 });
